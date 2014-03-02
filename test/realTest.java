700a5e958d69a8bca239b3a58ea670a958f7e428
uulake.com <wzy167356@gmail.com>
2014-03-03 00:22:46 +0800

    do more test

--- test/realTest.java
+++ test/realTest.java
@@ -2,6 +2,7 @@ public class RealTest{
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("welcome to m wondeful websiet");	
+		System.out.println("do somemore test");
 	}
 
 }
